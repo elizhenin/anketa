6454e814e5648182c2d5987fa7e8cc81164644a3
E.Lizhenin <elizhenin@lsbr.ru>
2016-11-10 16:54:51 +0300

    report

--- application/classes/Controller/Stat.php
+++ application/classes/Controller/Stat.php
@@ -41,8 +41,15 @@ public function before()
              * to = strtotime(to)-1;
              *
              */
-            $poliklinnika = Model_Anketa::GetPoliklinnika($from,$to);
+            $poliklinika = Model_Anketa::GetPoliklinnika($from,$to);
             $statsionar = Model_Anketa::GetPoliklinnika($from,$to);
+            $ods_content = View::factory('report/content-xml');
+            $ods_content->list1 = $poliklinika;
+            $ods_content->list2 = $statsionar;
+            $ods_content = $ods_content->render();
+            file_put_contents('report_template/content.xml',$ods_content);
+            self::Zip('report_template/','reports/'.time().'.ods');
+
         }
         $page = View::factory('report/index');
         $medorg = Model_Catalog::GetMedOrg(false);
@@ -56,5 +63,49 @@ public function before()
         $this->page = $page;
     }
 
+    private function Zip($source, $destination)
+    {
+        if (!extension_loaded('zip') || !file_exists($source)) {
+            return false;
+        }
+
+        $zip = new ZipArchive();
+        if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+            return false;
+        }
+
+        $source = str_replace('\\', '/', realpath($source));
+
+        if (is_dir($source) === true)
+        {
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+            foreach ($files as $file)
+            {
+                $file = str_replace('\\', '/', $file);
+
+                // Ignore "." and ".." folders
+                if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                    continue;
+
+                $file = realpath($file);
+
+                if (is_dir($file) === true)
+                {
+                    $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                }
+                else if (is_file($file) === true)
+                {
+                    $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                }
+            }
+        }
+        else if (is_file($source) === true)
+        {
+            $zip->addFromString(basename($source), file_get_contents($source));
+        }
+
+        return $zip->close();
+    }
 
 }
